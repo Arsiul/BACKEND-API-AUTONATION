44b7ec92bf3959a4ec3344b10e828bdd5e4f08a3
Arsiul <fernandobarrantes802@gmail.com>
2026-04-27 19:18:39 -0500

    Se añadio las funciones de Seleccionar todos los clientes y registrar nuevo en el controlador

--- src/models/client.model.js
+++ src/models/client.model.js
@@ -6,5 +6,15 @@ export class clientModel{
         const [rows] = await pool.query("SELECT * FROM tb_cliente")
         return rows
     }
+
+    static async Create(data){
+        const newClient = await pool.query(
+            `INSERT INTO tb_cliente(primer_nombre, segundo_nombre,primer_apellido,segundo_apellido,
+            dni,telefono,correo,direccion,url_img) VALUES (?,?,?,?,?,?,?,?,?)`,
+            [ data.primer_nombre, data.segundo_nombre ?? null, data.primer_apellido, data.segundo_apellido ?? null,
+            data.dni, data.telefono, data.correo, data.direccion, data.url_img ]
+        )
+        return newClient;
+    }
     
 }
