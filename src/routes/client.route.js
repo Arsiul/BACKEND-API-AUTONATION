a8e1ce3547fe5435562f43ee6e2e2c421ffed831
Arsiul <fernandobarrantes802@gmail.com>
2026-04-27 19:38:30 -0500

    Se añadió la nueva ruta con validaciones para registrar un nuevo cliente

--- src/routes/client.route.js
+++ src/routes/client.route.js
@@ -1,9 +1,11 @@
 import { clientController } from "../controllers/Client.controller.js";
+import { validateSchema } from "../middlewares/validate.middleware.js";
+import { clientSchema } from "../schemas/auth.schema.js";
 import { Router } from "express";
 
 const routerClient = Router()
 
 routerClient.get("/getAll", clientController.getAll)
-routerClient.post("/register", clientController.Register)
+routerClient.post("/register", validateSchema(clientSchema),clientController.Register)
 
 export default routerClient;
