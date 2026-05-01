bb826ea80a1267cc43beec61cc542d9dbf2c748d
Arsiul <fernandobarrantes802@gmail.com>
2026-05-01 17:29:37 -0500

    Esquemas de Validacion de datos de usuario y cliente

--- src/schemas/auth.schema.js
+++ src/schemas/auth.schema.js
@@ -17,3 +17,17 @@ export const clientSchema = z.object({
   direccion: z.string().min(5, "Dirección muy corta"),
   url_img: z.string().url("Debe ser una URL válida"),
 });
+
+export const userSchema = z.object({
+  primer_nombre: z.string().min(2),
+  segundo_nombre: z.string().optional(),
+  primer_apellido: z.string().min(2),
+  segundo_apellido: z.string().optional(),
+  dni: z.string().length(8),
+  correo: z.string().email(),
+  telefono: z.string().length(9),
+  url_img: z.string().url().optional(),
+  id_estado_usuario: z.number(),
+  id_rol: z.number(),
+  contrasena: z.string().min(1)
+});
