e7f26bcfa89787610203713f855619aa269b4ef4
Arsiul <fernandobarrantes802@gmail.com>
2026-05-01 18:43:41 -0500

    Función FindById agregada

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -14,4 +14,20 @@ export class UserControiller{
             })
         }
     }
+    static async findById(req,res){
+        const id = req.params.id
+        try {
+            const user = await UserModel.findById(id)
+            if(!user){
+                return res.status(404).json({
+                    message: "Usuario Inexistente"
+                })
+            }
+            return res.status(200).json(user);
+        } catch (error) {
+            return res.status(500).json({
+                message: "Error Interno"
+            })
+        }
+    }
 }
