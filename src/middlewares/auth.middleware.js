6496f1e7d5e4689f33d18b62ee1a39c24035269d
Arsiul <fernandobarrantes802@gmail.com>
2026-05-01 17:22:57 -0500

    Se añadió la validacion de Token recibido

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,19 @@
+import { verifyToken } from "../utils/jwt.js";
+
+export const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "No token" });
+  }
+
+  try {
+    const token = authHeader.split(" ")[1];
+    const decoded = verifyToken(token);
+
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Token inválido" });
+  }
+};
