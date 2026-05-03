d3c510a3460a95278a39546fc2534def818ab38c
Arsiul <fernandobarrantes802@gmail.com>
2026-05-02 21:15:40 -0500

    The missing validation was added to the protected route

--- src/routes/client.route.js
+++ src/routes/client.route.js
@@ -2,11 +2,12 @@ import { clientController } from "../controllers/client.controller.js";
 import { validateSchema } from "../middlewares/validate.middleware.js";
 import { clientSchema } from "../schemas/auth.schema.js";
 import { roleMiddleware } from "../middlewares/rol.middleware.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { Router } from "express";
 
 const clientroute = Router()
 
-clientroute.get("/getAll",roleMiddleware(1,2,3), clientController.getAll)
-clientroute.post("/register", roleMiddleware(1,2,3),validateSchema(clientSchema),clientController.Register)
+clientroute.get("/getAll",authMiddleware,roleMiddleware(1,2,3), clientController.getAll)
+clientroute.post("/register", authMiddleware,roleMiddleware(1,2,3),validateSchema(clientSchema),clientController.Register)
 
 export default clientroute;
