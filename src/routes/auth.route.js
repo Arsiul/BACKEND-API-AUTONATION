c9a2a009e5f232dffdcc30e9ada2799b8ecceab8
Arsiul <fernandobarrantes802@gmail.com>
2026-05-02 20:37:00 -0500

    Validations of permitted suns for protected routes were included

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller.js";
+import { roleMiddleware } from "../middlewares/rol.middleware.js"
 
 const authrouter = Router();
 
-authrouter.post("/register", AuthController.register);
+authrouter.post("/register", roleMiddleware(1),AuthController.register);
 authrouter.post("/login", AuthController.login);
 
 export default authrouter;
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validate.middleware.js";
-import { UserControiller } from "../controllers/user.controller.js";
+import { UserController } from "../controllers/user.controller.js";
 import { userSchema } from "../schemas/auth.schema.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { roleMiddleware } from "../middlewares/rol.middleware.js";
 
 
 const userrouter = Router()
 
-userrouter.get("/getAll", UserControiller.getAll)
+userrouter.get("/getAll", authMiddleware, roleMiddleware(1),UserController.getAll)
 
 
 
