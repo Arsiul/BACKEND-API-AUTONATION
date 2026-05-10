084313db5c5bd5e3db28f44af0dcd083b931c0bd
Arsiul <fernandobarrantes802@gmail.com>
2026-05-10 14:24:19 -0500

    Zod bug fix

--- src/schemas/auth.schema.js
+++ src/schemas/auth.schema.js
@@ -2,9 +2,9 @@ import { z } from "zod";
 
 export const clientSchema = z.object({
   primer_nombre: z.string().min(3, "Debe tener al menos 3 caracteres"),
-  segundo_nombre: z.string().min(3, "Debe tener al menos 3 caracteres").nullable(),
+  segundo_nombre: z.string().min(3, "Debe tener al menos 3 caracteres").nullable().optional(),
   primer_apellido: z.string().min(3, "Debe tener al menos 3 caracteres"),
-  segundo_apellido: z.string().min(3, "Debe tener al menos 3 caracteres").nullable(),
+  segundo_apellido: z.string().min(3, "Debe tener al menos 3 caracteres").nullable().optional(),
   dni: z.string()
     .min(8, "DNI inválido")
     .max(8, "DNI inválido")
@@ -15,18 +15,18 @@ export const clientSchema = z.object({
     .regex(/^[0-9]+$/, "Solo números"),
   correo: z.string().email("Correo inválido"),
   direccion: z.string().min(5, "Dirección muy corta"),
-  url_img: z.string().url("Debe ser una URL válida").nullable(),
+  url_img: z.string().url("Debe ser una URL válida").nullable().optional(),
 });
 
 export const userSchema = z.object({
   primer_nombre: z.string().min(2),
-  segundo_nombre: z.string().nullable(),
+  segundo_nombre: z.string().nullable().optional(),
   primer_apellido: z.string().min(2),
-  segundo_apellido: z.string().nullable(),
+  segundo_apellido: z.string().nullable().optional(),
   dni: z.string().length(8),
   correo: z.string().email(),
   telefono: z.string().length(9),
-  url_img: z.string().url().nullable(),
+  url_img: z.string().url().nullable().optional(),
   id_estado_usuario: z.number(),
   id_rol: z.number(),
   contrasena: z.string().min(1)
