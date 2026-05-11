2978f1b8564d16d68600f4b0279f7c5db9591d0c
Arsiul <fernandobarrantes802@gmail.com>
2026-05-11 03:08:21 -0500

    SQL query change for sales records

--- src/models/sale.model.js
+++ src/models/sale.model.js
@@ -2,16 +2,33 @@ import { pool } from "../config/db.js"
 
 export class SaleModel{
 
-    static async getAll(){
-        const [result] = await pool.query(
-            `SELECT tb1.id,tb1.serie,tb1.nro_comprobante,tb1.fecha_venta,tb1.subtotal,tb1.igv,tb1.total,tb1.id_estado_pago,tb1.id_cliente,tb1.id_tipo_comprobante,tb1.id_usuario, tb2.id_vehiculo,
-tb3.anio, tb3.precio_u, tb3.url_img 
-FROM tb_venta tb1
-INNER JOIN tb_detalle_venta tb2 ON (tb1.id = tb2.id_venta)
-INNER JOIN tb_vehiculo tb3 ON (tb2.id_vehiculo = tb3.id) ORDER BY nro_comprobante DESC;`
-        )
-        return result;
-    }
+static async getAll(){
+    const [result] = await pool.query(`
+        SELECT tb1.id,tb1.serie,tb1.nro_comprobante,tb1.fecha_venta,
+        tb1.subtotal,tb1.igv,tb1.total,tb1.id_estado_pago,
+        tb1.id_cliente,tb1.id_tipo_comprobante,tb1.id_usuario,
+        JSON_ARRAYAGG(
+            JSON_OBJECT(
+                'id_vehiculo',tb3.id,
+                'anio',tb3.anio,
+                'precio_u',tb3.precio_u,
+                'url_img',tb3.url_img
+            )
+        ) AS vehiculos
+        FROM tb_venta tb1
+        INNER JOIN tb_detalle_venta tb2 ON tb1.id = tb2.id_venta
+        INNER JOIN tb_vehiculo tb3 ON tb2.id_vehiculo = tb3.id
+        GROUP BY tb1.id,tb1.serie,tb1.nro_comprobante,tb1.fecha_venta,
+        tb1.subtotal,tb1.igv,tb1.total,tb1.id_estado_pago,
+        tb1.id_cliente,tb1.id_tipo_comprobante,tb1.id_usuario
+        ORDER BY tb1.nro_comprobante DESC;
+    `)
+
+    return result.map(row => ({
+        ...row,
+        vehiculos: JSON.parse(row.vehiculos)
+    }))
+}
     static async createSale(conn, data) {
         await conn.query(
         `INSERT INTO tb_venta 
