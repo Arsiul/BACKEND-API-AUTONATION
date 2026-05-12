6535ec5822d4146fb54ff92e1d687f5558b4dadc
Arsiul <fernandobarrantes802@gmail.com>
2026-05-12 03:04:59 -0500

    New Change added

--- src/models/buy.model.js
+++ src/models/buy.model.js
@@ -3,7 +3,7 @@ import { pool } from "../config/db.js";
 export class BuyModel{
     static async getAll(){
         const [result] = await pool.query(
-            `SELECT * FROM tb_compra`
+            `SELECT tb1.id, tb1.serie, tb1.nro_comprobante, tb1.fecha_compra,tb1.subtotal, tb1.igv, tb1.total, tb1.id_moneda, tb1.id_estado_pago, tb1.id_proveedor, tb1.id_tipo_comprobante, tb2.ruc, tb2.razon_social, tb2.email, tb2.direccion, tb2.telefono FROM tb_compra tb1 INNER JOIN tb_proveedor tb2 ON (tb1.id_proveedor = tb2.id)`
         );
         return result;
     }
